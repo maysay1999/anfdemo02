daea746a431f4dc2cf82893e00457702311edc52
Meisei Takemoto (NETAPP INC) <meisei@pobox.com>
2022-03-02 13:06:54 +0900

    japanwest-create.sh is executor ver 1.1

--- japanwest-create.sh
+++ japanwest-create.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-az network vnet create -g anfdemo-rg -n anfjpw-vnet \
+az network vnet create -g anfdemolab-rg -n anfjpw-vnet \
     --address-prefix 172.21.0.0/16 \
     --subnet-name vm-subnet \
     -l japanwest \
     --subnet-prefix 172.21.0.0/24
 
 az network vnet subnet create \
-    --resource-group anfdemo-rg \
+    --resource-group anfdemolab-rg \
     --vnet-name anfjpw-vnet \
     --name anf-subnet \
     --delegations "Microsoft.NetApp/volumes" \
     --address-prefixes 172.21.1.0/26
 
 az netappfiles account create \
-    -g anfdemo-rg \
+    -g anfdemolab-rg \
     --name anfjpw -l japanwest
 
 az netappfiles pool create \
-    --resource-group anfdemo-rg \
+    --resource-group anfdemolab-rg \
     --location japanwest \
     --account-name anfjpw \
     --pool-name pooldr \
